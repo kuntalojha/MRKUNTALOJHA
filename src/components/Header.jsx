57b91e7f672c92ba90f9b4e8d0c0f613f18c64cb
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-02-09 00:56:46 +0530

    fix: restore original logo image path in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
         <div className='max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]'>
           <h1>
             <a href='/' className='logo'>
-              {/* <img src='./images/logo.png' width={40} height={40} alt='logo' /> */}
-              <img src='../images/logo.png' width={40} height={40} alt='logo' />
+              <img src='./images/logo.png' width={40} height={40} alt='logo' />
+              {/* <img src='../images/logo.png' width={40} height={40} alt='logo' /> */}
             </a>
           </h1>
 
