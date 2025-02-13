5e1cc8a697f5a44b6cb0bfb1be40bb237adcf538
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-02-13 15:00:33 +0530

    feat: enhance Footer component with improved layout and additional social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -52,21 +52,75 @@ const socials = [
     label: 'CodePen',
     href: 'https://codepen.io/kuntalojha',
   },
+  {
+    label: 'Website',
+    href: 'https://kuntalojha.github.io/MRKUNTALOJHA/',
+  },
 ];
 
 const Footer = () => {
   return (
     <footer className='section'>
       <div className='container'>
-        <div className=''>
-          <div className=''>
-            <h2 className='headline-1'>Let&apos;s work together!</h2>
+        <div className='lg:grid lg:grid-cols-2 '>
+          <div className='mb-10'>
+            <h2 className='headline-2 mb-8 lg:max-w-[12ch]'>
+              Let&apos;s work together & grow together
+            </h2>
             <ButtonPrimary
               href='mailto:kuntalojha26398@gmail.com'
               label='Start project'
               icon={<FaChevronRight />}
             />
           </div>
+
+          <div className='grid grid-cols-2 gap-4 lg:pl-20 '>
+            <div>
+              <p className='mb-4'>Sitemap</p>
+
+              <ul>
+                {sitemap.map(({ label, href }, key) => (
+                  <li key={key}>
+                    <a
+                      href={href}
+                      className='block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200'
+                    >
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div>
+              <p className='mb-4'>Socials</p>
+
+              <ul>
+                {socials.map(({ label, href }, key) => (
+                  <li key={key}>
+                    <a
+                      href={href}
+                      target='_blank'
+                      className='block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200'
+                    >
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className='flex items-center justify-between pt-10 mb-8 '>
+          <a href='' className=''>
+            <img src='./images/logo.png' alt='Logo' width={40} height={40} />
+          </a>
+
+          <p className='text-zinc-500 text-sm'>
+            &copy; {new Date().getFullYear()}{' '}
+            <span className='text-zinc-200'> by Kuntal Ojha</span>
+          </p>
         </div>
       </div>
     </footer>
