00f2f1b719596aad5628ece4d0bc1941c0103e64
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-02-13 22:05:33 +0530

    refactor: clean up Button component code and enhance button styles in CSS

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,70 +1,12 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+// Desc: Button Component
+// Name: KUNTAL OJHA
+// Date: 13-02-2025
 
-// import { IoMdDownload } from 'react-icons/io';
-// import { IoArrowDownOutline } from 'react-icons/io5';
-
-
-// const ButtonPrimary = ({ href, target = '_self', label, icon, classes }) => {
-//   if (href) {
-//     return (
-//       <a href={href} target={target} className={'btn btn-primary ' + classes}>
-//         {label}
-//         {icon ? <IoMdDownload className='icon' /> : undefined}
-//       </a>
-//     );
-//   } else {
-//     return (
-//       <button className={'btn btn-primary ' + classes}>
-//         {label}
-//         {icon ? <IoMdDownload className='icon cv' /> : undefined}
-//       </button>
-//     );
-//   }
-// };
-
-// ButtonPrimary.propTypes = {
-//   label: PropTypes.string.isRequired,
-//   href: PropTypes.string,
-//   target: PropTypes.string,
-//   icon: PropTypes.string,
-//   classes: PropTypes.string,
-// };
-
-// //Outline Button
-// const ButtonOutline = ({ href, target = '_self', label, icon, classes }) => {
-//   if (href) {
-//     return (
-//       <a href={href} target={target} className={'btn btn-outline ' + classes}>
-//         {label}
-//         {icon ? <IoArrowDownOutline className='icon' /> : undefined}
-//       </a>
-//     );
-//   } else {
-//     return (
-//       <button className={'btn btn-outline ' + classes}>
-//         {label}
-//         {icon ? <IoArrowDownOutline className='icon cv' /> : undefined}
-//       </button>
-//     );
-//   }
-// };
-
-// ButtonOutline.propTypes = {
-//   label: PropTypes.string.isRequired,
-//   href: PropTypes.string,
-//   target: PropTypes.string,
-//   icon: PropTypes.string,
-//   classes: PropTypes.string,
-// };
-
-// export { ButtonPrimary, ButtonOutline };
-
-
+// Node modules
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
+//Primary Button
 const ButtonPrimary = ({ href, target = '_self', label, icon, classes }) => {
   if (href) {
     return (
